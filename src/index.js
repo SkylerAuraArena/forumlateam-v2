338fe1d99c2053ea9105e6e94b65a1bcbba75b0c
SkylerAuraArena <skylerauraarena@gmail.com>
2022-05-20 17:24:37 +0200

    Initialisation terminée mais pas corrigée

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
@@ -11,7 +11,8 @@ import FirebaseConnectionProvider from './components/context/FirebaseConnectionC
 
 console.log("%cIndex.js", 'background: black; color: white;')
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
@@ -26,6 +27,5 @@ ReactDOM.render(
         </ForumContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
